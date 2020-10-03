cb27e0bddf04a6abb5945fd61cc3b53052217f29
SiJiL82 <sven.lowry@gmail.com>
2020-10-03 08:56:10 +0100

    Added play button to start menu

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    private bool _hasBeenClicked;
+    public bool hasBeenClicked {get{return _hasBeenClicked;} private set{_hasBeenClicked = value;}}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
